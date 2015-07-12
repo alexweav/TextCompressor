730d27c0f18017a70a59bf1d4679540c56faad36
alexweav <alexander.weaver@ttu.edu>
2015-07-12 18:11:31 -0500

    Initial commit

---
+++ UserInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCompressor {
+
+    //Text-based user interface for TextCompressor which works in-console
+    //Responsible for obtaining information from and interacting with the user
+    class UserInterface {
+
+        public void init() {
+            printHeader();
+            programLoop();
+        }
+
+        private void printHeader() {
+            Console.WriteLine("---Welcome to TextCompressor---");
+        }
+
+        private void programLoop() {
+
+        }
+    }
+}
