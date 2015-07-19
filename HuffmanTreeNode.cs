12752dc17c0eb42a345055c7202807a8b88728c1
alexweav <alexander.weaver@ttu.edu>
2015-07-18 20:59:57 -0500

    Huffman Tree Node structure implemented

---
+++ HuffmanTreeNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCompressor.Source {
+    class HuffmanTreeNode {
+
+        private string charset = "";
+        private HuffmanTreeNode left = null;
+        private HuffmanTreeNode right = null;
+
+        //constructor
+        public HuffmanTreeNode(string charset, HuffmanTreeNode left, HuffmanTreeNode right) {
+            this.charset = charset;
+            this.left = left;
+            this.right = right;
+        }
+
+        //get/set
+        public string Charset {
+            get { return charset; }
+            set { this.charset = value; }
+        }
+
+        public HuffmanTreeNode Left {
+            get { return left; }
+            set { this.left = value; }
+        }
+
+        public HuffmanTreeNode Right {
+            get { return right; }
+            set { this.right = value; }
+        }
+    }
+}
