579a16dcb1e44c0680167bce45e68d00aaa1e7ac
alexweav <alexander.weaver@ttu.edu>
2015-08-03 13:01:04 -0700

    Exception handling

--- BinaryStream.cs
+++ BinaryStream.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace TextCompressor.Source
 {
+    //BinaryStream data type
+    //Constructed from an array of bytes, assumed to be raw binary data
+    //Functions like an input stream, starting from the beginning of the data
+    //Reads individual bits or entire bytes, advancing along the stream in a *forward only* direction until the stream ends
+    //Bit and byte reading ignores byte boundaries in the given data
     class BinaryStream
     {
         private const int BYTE_LENGTH = 8;
@@ -13,6 +18,7 @@ namespace TextCompressor.Source
         private byte[] data;
         private int byteIndex;
         private int bitIndex;
+        private Boolean endOfStream;
 
         #region constructors
         public BinaryStream() {
@@ -24,22 +30,45 @@ namespace TextCompressor.Source
             if (this.data.Length > 0) {
                 byteIndex = 0;
                 bitIndex = 0;
+                endOfStream = false;
+            } else {
+                endOfStream = true;
             }
         }
         #endregion
 
+        public Boolean isEndOfStream() {
+            return endOfStream;
+        }
+
         //Reads a single bit from the data stream, returns a 0 or 1 in byte length reflecting the bit's state
         public byte readBit() {
+            if (isEndOfStream()) {
+                throw new IndexOutOfRangeException("Stream has ended.");
+            }
             byte currentBit = getCurrentBit();
-            changeIndices(1);
+            try {
+                changeIndices(1);
+            } catch (Exception) {
+                endOfStream = true;
+            }
             return currentBit;
         }
 
         //Reads 8 bits from the data stream, and returns the data 
         public byte readByte() {
+            int initialByteIndex = byteIndex;
+            int initialBitIndex = bitIndex;
             byte output = 0;
             for (byte i = 0; i < BYTE_LENGTH; ++i) {
-                byte bit = readBit();
+                byte bit;
+                try {
+                    bit = readBit();
+                } catch (Exception) {
+                    byteIndex = initialByteIndex;
+                    bitIndex = initialBitIndex;
+                    throw new IndexOutOfRangeException("Stream has ended.");
+                }
                 bit = (byte)(bit << i);
                 output = (byte)(output | bit);
             }
